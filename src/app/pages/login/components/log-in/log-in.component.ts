445c79edfd5bf3e755ac7dcb2bd6c530003d45bf
Mykola <developersame@gmail.com>
2018-09-30 20:03:08 +0300

    'class-transformer

--- src/app/pages/login/components/log-in/log-in.component.ts
+++ src/app/pages/login/components/log-in/log-in.component.ts
@@ -1,4 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {plainToClass, Type} from 'class-transformer';
+
+export class Album {
+
+  id: number;
+
+  name: string;
+
+  @Type(() => Photo)
+  photos: Photo[];
+}
+
+
+export class Photo {
+  id: number;
+  filename: string;
+}
 
 @Component({
   selector: 'app-log-in',
@@ -6,10 +23,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent implements OnInit {
-
+  data: Album;
   constructor() { }
 
   ngOnInit() {
+
+    const newData = {
+      id: 36,
+      filename: 'Ping',
+      photos: [
+        {
+          id: 78,
+          filename: 'box'
+        },
+        {
+          id: 77,
+          filename: 'box-77'
+        },
+        {
+          id: 79,
+          filename: 'box-79'
+        }
+      ]
+    };
+    this.data = plainToClass(Album, newData);
+    console.log(this.data)
   }
 
 }
